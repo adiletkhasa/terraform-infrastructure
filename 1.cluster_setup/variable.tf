00b5bbbb8c560d8a05b2675be23960889c42e003
adilet khasanov <akhasa2001@gmail.com>
2024-03-28 08:52:09 -0500

    changed the cluster name

--- 1.cluster_setup/variable.tf
+++ 1.cluster_setup/variable.tf
@@ -4,7 +4,7 @@ variable "gke_config" {
   default = {
     region         = "us-central1"
     zone           = "us-central1-c"
-    cluster_name   = "project-new"
+    cluster_name   = "project-cluster"
     machine_type   = "e2-medium"
     node_count     = 1
     node_pool_name = "my-node-pool"
