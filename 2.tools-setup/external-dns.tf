dba959078ccbc19a466c9225e804b63fd25be987
Adilet Khasanov <92490823+akhasa2001@users.noreply.github.com>
2024-04-03 17:54:45 +0000

    added external DNS deployment code

--- 2.tools-setup/external-dns.tf
+++ 2.tools-setup/external-dns.tf
@@ -3,23 +3,58 @@ module "external-dns-terraform-k8s-namespace" {
   name   = "external-dns"
 }
 
+
 resource "google_service_account" "external-dns" {
-  account_id   = "project-external-dns"
-  display_name = "Service Account"
+  account_id   = "pro-external-dns"
+  display_name = "Used for external-dns"
   project      = var.PROJECT_ID
 }
 
+resource "google_service_account_key" "external-dns" {
+  service_account_id = google_service_account.external-dns.name
+  public_key_type    = "TYPE_X509_PEM_FILE"
+}
+
+
+resource "google_project_iam_binding" "externald-dns" {
+  project = var.PROJECT_ID
+  role    = "roles/dns.admin"
+  members = [
+    "serviceAccount:${google_service_account.external-dns.email}"
+  ]
+}
+
+
+
+resource "kubernetes_secret" "external_dns_secret" {
+  metadata {
+    name      = "external-dns"
+    namespace = module.external-dns-terraform-k8s-namespace.namespace
+  }
+  data = {
+    "credentials.json" = base64decode(google_service_account_key.external-dns.private_key)
+  }
+  type = "generic"
+}
+
+
+
 module "external-dns-terraform-helm" {
+  depends_on = [
+    kubernetes_secret.external_dns_secret
+  ]
   source               = "../modules/terraform-helm/"
   deployment_name      = "external-dns"
-  deployment_namespace = module.ingress-terraform-k8s-namespace.namespace
+  deployment_namespace = module.external-dns-terraform-k8s-namespace.namespace
   chart                = "external-dns"
   chart_version        = var.external-dns-config["chart_version"]
   repository           = "https://charts.bitnami.com/bitnami"
   values_yaml          = <<EOF
-  provider: google
-  google:
-    project: "${var.PROJECT_ID}"
-    serviceAccountSecret: ""
-  EOF
+provider: google
+google:
+  project: "${var.PROJECT_ID}"
+  serviceAccountSecret: external-dns 
+rbac:
+  create: true
+EOF
 }
