f72924ccb38cab6bff54eb57ab4febfa1b95752c
adilet khasanov <akhasa2001@gmail.com>
2024-04-09 11:05:07 -0500

    updated servie name for argo

--- 2.tools-setup/monitoring.tf
+++ 2.tools-setup/monitoring.tf
@@ -9,6 +9,6 @@ module "argo" {
   source             = "../modules/monitoring"
   google_domain_name = var.google_domain_name
   PROJECT_ID         = var.PROJECT_ID
-  service_name       = "argo-cd"
+  service_name       = "argo"
 }
 
