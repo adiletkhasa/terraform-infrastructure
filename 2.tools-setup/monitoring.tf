43e4f370e2cfa5cb4783469227bc753d5ce0960e
adilet khasanov <akhasa2001@gmail.com>
2024-04-09 10:01:39 -0500

    added monitoring

---
+++ 2.tools-setup/monitoring.tf
@@ -0,0 +1,14 @@
+module "vault" {
+  source             = "../modules/monitoring"
+  google_domain_name = var.google_domain_name
+  PROJECT_ID         = var.PROJECT_ID
+  service_name       = "dev-vault"
+}
+
+module "argo" {
+  source             = "../modules/monitoring"
+  google_domain_name = var.google_domain_name
+  PROJECT_ID         = var.PROJECT_ID
+  service_name       = "argo-cd"
+}
+
