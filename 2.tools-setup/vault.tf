8c9ad2b9ab9674bab63196abca5af6c69132d79c
adilet khasanov <akhasa2001@gmail.com>
2024-04-09 10:30:04 -0500

    updated my vault configurations

--- 2.tools-setup/vault.tf
+++ 2.tools-setup/vault.tf
@@ -11,29 +11,30 @@ module "vault-terraform-helm" {
   repository           = "https://helm.releases.hashicorp.com"
   chart_version        = var.vault-config["chart_version"]
   values_yaml          = <<-EOF
-server:
-    ingress:
-      enabled: true
-      annotations: 
-        nginx.ingress.kubernetes.io/proxy-body-size: "0"
-        ingress.kubernetes.io/ssl-redirect: "false"
-        acme.cert-manager.io/http01-edit-in-place: "true"
-        cert-manager.io/cluster-issuer: letsencrypt-dev
-      ingressClassName: "nginx"
-      hosts:
-      - host: "dev-vault.${var.google_domain_name}"
-        http:
-          paths: 
-          - pathType: Prefix
-            path: "/"
-            backend:
-              service:
-                name: vault
-                port:
-                  number: 8200
-      tls:
-       - secretName: vault-tls
-         hosts:
+server:  
+  ingress:
+    enabled: true
+    annotations: 
+      nginx.ingress.kubernetes.io/proxy-body-size: "0"
+      ingress.kubernetes.io/ssl-redirect: "false"
+      cert-manager.io/cluster-issuer: letsencrypt-dev
+      acme.cert-manager.io/http01-edit-in-place: "true"
+    ingressClassName: "nginx"
+    hosts:
+    - host: "dev-vault.${var.google_domain_name}"
+      http:
+        paths:
+        - pathType: Prefix
+          path: "/"
+          backend:
+            service:
+              name: vault
+              port:
+                number: 8200
+      
+    tls: 
+      - secretName: vault-tls
+        hosts:
           - "dev-vault.${var.google_domain_name}"
   EOF
 }
