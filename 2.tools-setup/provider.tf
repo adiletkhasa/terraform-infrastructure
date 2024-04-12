c78cc57ace34dc0ad435c0e89c4a594f27d79f92
adilet khasanov <akhasa2001@gmail.com>
2024-04-12 11:06:08 -0500

    added prometheus file and updated providers for vault

--- 2.tools-setup/provider.tf
+++ 2.tools-setup/provider.tf
@@ -11,4 +11,5 @@ provider "helm" {
 provider "vault" {
     address = "https://dev-vault.${var.google_domain_name}/"
     token = file(pathexpand("${path.module}/vault-token.txt"))
+    skip_tls_verify = true
 }
