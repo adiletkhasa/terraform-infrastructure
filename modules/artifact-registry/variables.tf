81cadab5a0589ffffe841de9329b31f6a56b9d20
adilet khasanov <akhasa2001@gmail.com>
2024-04-22 11:48:38 -0500

    fixed the repositry name

--- modules/artifact-registry/variables.tf
+++ modules/artifact-registry/variables.tf
@@ -3,6 +3,6 @@ variable artifact-config {
   description = "configurations for Artifact repo"
   default     = {
     location      = "us-central1"
-    repository_id = "docker_registry"
+    repository_id = "docker-registry"
   }
 }
